9ca12c6bd4bffeddf2b5b7eb623655848a7bce64
Adrian Shina <adrianshina@gmail.com>
2019-10-12 22:51:11 -0700

    second commit

--- src/test/java/edu/gcccd/csis/ApproximatePITest.java
+++ src/test/java/edu/gcccd/csis/ApproximatePITest.java
@@ -3,15 +3,11 @@ package edu.gcccd.csis;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ApproximatePITest {
 
     @Test
     public void value() {
-        for (int i = 0; i < 20; i++) {
-            float value = ApproximatePI.value();
-            Assert.assertEquals(Math.PI, value, 0.00001);
-        }
+        float value = ApproximatePI.value();
+        Assert.assertEquals(Math.PI, value, 0.00001);
     }
 }

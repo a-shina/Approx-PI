fa29337b6d62bb165422bcaa1a5e9b4e7264105d
Adrian Shina <adrianshina@gmail.com>
2019-10-12 21:14:05 -0700

    first commit

---
+++ src/main/java/edu/gcccd/csis/ApproximatePI.java
@@ -0,0 +1,38 @@
+package edu.gcccd.csis;
+
+import java.util.Random;
+
+public class ApproximatePI {
+    public static void main(String[] args) {
+        System.out.println(value());
+        System.out.println(Math.PI);
+    }
+
+    public static float value() {
+        float sqPoints = 0;
+        float cirPoints = 0;
+        final double MIN_PI = 3.1415910;
+        final double MAX_PI = 3.1415899;
+
+        Random rd = new Random();
+        float PI;
+
+        while (true) {
+            float x = rd.nextFloat();
+            float y = rd.nextFloat();
+            float coordinate = x * x + y * y;
+            sqPoints++;
+
+            if (coordinate < 1) {
+                cirPoints++;
+            }
+            float ratio = cirPoints / sqPoints;
+            PI = ratio * 4;
+            if (PI <= MIN_PI && PI >= MAX_PI) {
+                break;
+            }
+        }
+        return PI;
+    }
+}
+
